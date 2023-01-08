42fb56d25d97fa4b8ef80efd736742d00462d710
Santosh <san.kr.sharma090@gmail.com>
2023-01-08 14:08:57 +0530

    check

--- Day-2movie-app/pages/index.jsx
+++ Day-2movie-app/pages/index.jsx
@@ -13,7 +13,7 @@ const Page = ({ blogs = [] }) => {
   return (
     <>
       <Head>
-        <title>Blogs Page</title>
+        <title>Blogs Pages</title>
         <meta titile="description" content="blogs,react.js,typescript"></meta>
       </Head>
       <main>
